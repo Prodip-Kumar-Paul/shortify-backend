aafd67f0097916cdbfca45a0014e95d05d4a2cb1
Prodip-Kumar-Paul <prodipkumarpaul708@gmail.com>
2023-01-19 13:00:49 +0530

    initial setup

---
+++ config/config.dev.js
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+dotenv.config();
+import { fileURLToPath } from "node:url";
+import path, { dirname } from "node:path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const ENV = process.env.NODE_ENV || "prod";
+
+const configDev = {
+  //HOST: process.env.DEV_HOST,
+  PORT: process.env.DEV_PORT,
+  DB_NAME: process.env.DEV_DB_NAME,
+  DB_URL: process.env.DEV_DB_URL,
+  DB_PASSWORD: process.env.DEV_DB_PASSWORD,
+};
+
+export default configDev;
---
+++ config/config.prod.js
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+dotenv.config();
+import { fileURLToPath } from "node:url";
+import path, { dirname } from "node:path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const ENV = process.env.NODE_ENV || "prod";
+
+const configProd = {
+   //HOST: process.env.PROD_HOST,
+   PORT: process.env.PROD_PORT,
+   DB_NAME: process.env.PROD_DB_NAME,
+   DB_URL: process.env.PROD_DB_URL,
+   DB_PASSWORD: process.env.PROD_DB_PASSWORD,
+};
+
+export default configProd;
